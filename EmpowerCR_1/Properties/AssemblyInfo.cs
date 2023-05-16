fba898f7611360e128f748925cc6848d7d3d1479
robackc <robackc@40GC3D3>
2023-05-16 14:09:24 -0400

    Add project files.

---
+++ EmpowerCR_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerCR_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerCR_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8E259CC1-EAC4-46DB-9BE6-16A77BEF21DE")]
+[assembly: AssemblyVersion("1.0.0.0")]
